017b048fa42bfc0260d8183307c99a3223f6db12
Michael Carr <mcarr2468@gmail.com>
2023-08-30 19:20:33 -0500

    refactor by adding btn-main a few other small changes

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link, useRouteError } from 'react-router-dom'
+import React from "react"
+import { Link, useRouteError } from "react-router-dom"
 
 import pageNotFound from "../assets/images/page_not_found.svg"
 import serverDown from "../assets/images/server-down.svg"
@@ -10,20 +10,20 @@ const Error = () => {
   if(error.status === 404) {
     return (
       <div className="min-h-[100vh] text-center flex flex-col justify-center items-center">
-        <img src={pageNotFound} className='w-[90vw] max-w-[600px]' alt="not found"/>
-        <h2 className='text-4xl mt-8'>Page not found.</h2>
-        <p className='text-gray-500 mt-2 mb-4'>Apologies, but it seems like we couldn't find the page you're looking for.</p>
-        <Link to="/" className='bg-green-500 border-green-500 py-4 px-6 text-white rounded-sm'>Go home</Link>
+        <img src={pageNotFound} className="w-[90vw] max-w-[600px]" alt="not found"/>
+        <h2 className="text-4xl mt-8">Page not found.</h2>
+        <p className="text-gray-500 mt-2 mb-4">Apologies, but it seems like we couldn't find the page you're looking for.</p>
+        <Link to="/" className="btn-main py-4 px-6">Go home</Link>
       </div>
     )
   }
 
   return (
     <div className="min-h-[100vh] text-center flex flex-col justify-center items-center">
-      <img src={serverDown} className='w-[90vw] max-w-[600px]' alt="not found"/>
-      <h2 className='text-4xl mt-8'>An Error has Occurred.</h2>
-      <p className='text-gray-500 mt-2 mb-4'>We apologize for any disruption this may have caused to your experience..</p>
-      <Link to="/" className='bg-green-500 border-green-500 py-4 px-6 text-white rounded-sm'>Go home</Link>
+      <img src={serverDown} className="w-[90vw] max-w-[600px]" alt="not found"/>
+      <h2 className="text-4xl mt-8">An Error has Occurred.</h2>
+      <p className="text-gray-500 mt-2 mb-4">We apologize for any disruption this may have caused to your experience..</p>
+      <Link to="/" className="btn-main py-4 px-6">Go home</Link>
     </div>
   )
 }
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-import img123 from "../assets/images/avatar-2.jpg"
+import React from "react"
+import { Link } from "react-router-dom"
 
 const Landing = () => {
   let headingText = ", Your Effortless Inventory Management Solution"
@@ -12,27 +10,27 @@ const Landing = () => {
 
   return (
     <div className="grid place-items-center h-[100vh]">
-      <div className='sm:max-w-4xl max-w-full text-center'>
-        <div className='sm:mx-8 mx-0'>
+      <div className="sm:max-w-4xl max-w-full text-center">
+        <div className="sm:mx-8 mx-0">
           <h1 className="text-5xl font-medium leading-tight">
-            <span>Stock<span className='text-green-500'>Optima</span></span>
+            <span>Stock<span className="text-green-500">Optima</span></span>
             {headingText}
           </h1>
-          <p className='my-6'>{descriptionText}</p>
+          <p className="my-6">{descriptionText}</p>
           
-          <div className='p-4 flex justify-center items-center flex-col sm:flex-row sm:space-x-4 space-x-0 space-y-4 sm:space-y-0'>
-            <Link to="/register" className='bg-green-500 p-4 text-white rounded-sm'>
+          <div className="p-4 flex justify-center items-center flex-col sm:flex-row sm:space-x-4 space-x-0 space-y-4 sm:space-y-0">
+            <Link to="/register" className="btn-main">
             Register
             </Link>
-            <Link to="login" className='bg-green-500 p-4 text-white rounded-sm'>
+            <Link to="login" className="btn-main">
             Login / demo user
             </Link>
           </div>
         </div>
 
-        <div className='mt-10 w-full'>
+        <div className="mt-10 w-full">
           {/* make sure when you've finished the actual dashboard & populated it w/ data to replace the placeholder here w/ real screenshot */}
-          <img src={imgDisplay} alt="dashboard of stock optima" className='rounded-sm max-h-[500px] w-full object-cover drop-shadow'/>
+          <img src={imgDisplay} alt="dashboard of stock optima" className="rounded-sm max-h-[500px] w-full object-cover drop-shadow"/>
         </div>
       </div>
 
