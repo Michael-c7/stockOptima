78f3153b37472d319aa4c66353bfead7a6592202
Michael Carr <mcarr2468@gmail.com>
2023-08-30 19:19:06 -0500

    add styles

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
+import InputContainer from "../../components/InputContainer"
+
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to="/register">register page</Link>
-    </div>
+    <section className="min-h-[100vh] text-center flex flex-col justify-center items-center bg-gray-50 ">
+      <form method="post" action="/register" className="bg-white px-4 py-6 w-[90vw] max-w-sm drop-shadow-sm">
+        <h1 className="text-4xl mb-8 font-light capitalize">Login</h1>
+        <InputContainer type="text" name="name" labelText="name" />
+
+        <InputContainer type="password" name="password" labelText="password" />
+
+        <button type="submit" className="btn-main w-full">Submit</button>
+        <button type="button" className="btn-main w-full mt-2">Explore the App</button>
+
+        <p className="mt-3">Don't have an account? <Link to="/register" className="text-green-500">Sign up</Link></p>
+      </form>
+    </section>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
+import InputContainer from "../../components/InputContainer"
+
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">login page</Link>
-    </div>
+    <section className="min-h-[100vh] text-center flex flex-col justify-center items-center bg-gray-50 ">
+      <form method="post" action="/register" className="bg-white px-4 py-6 w-[90vw] max-w-sm drop-shadow-sm">
+        <h1 className="text-4xl mb-8 font-light capitalize">Register</h1>
+        <InputContainer type="text" name="name" labelText="name" />
+        <InputContainer type="password" name="password" labelText="password" />
+
+        <button type="submit" className="btn-main w-full">Submit</button>
+        <p className="mt-3">Have an account? <Link to="/login" className="text-green-500">Login</Link></p>
+      </form>
+    </section>
   )
 }
 
