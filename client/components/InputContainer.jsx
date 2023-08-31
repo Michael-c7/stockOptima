9fe0f7648bc71ea7aa5f3df865b877c11ca51f54
Michael Carr <mcarr2468@gmail.com>
2023-08-30 19:18:30 -0500

    create component for input containers

---
+++ client/components/InputContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputContainer = ({ type, name, labelText }) => {
+  return (
+    <div className="flex flex-col text-left my-4">
+        <label htmlFor={name} className='font-light capitalize mb-1'>{labelText || name}</label>
+        <input type={type} id={type} name={type} required className="border rounded border-gray-200 p-1 w-full"/>
+    </div>
+  )
+}
+
+export default InputContainer
