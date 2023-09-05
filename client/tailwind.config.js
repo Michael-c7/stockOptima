28586fed602ce13ca073f9481d66b7d25af162d7
Michael Carr <mcarr2468@gmail.com>
2023-09-05 10:30:21 -0500

    add components folder

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,19 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+
+/*
+Make sure to add the whatever folder you to 
+apply style to in the content
+array in the current tailwind.config.js file
+otherwise you might get a weird bug where
+some tailwind styles don't work
+*/
