b29483a1aceb7a334fcf8c84e825799cb59056e0
Michael Carr <mcarr2468@gmail.com>
2023-09-07 20:44:08 -0500

    add connection to the database and abilty to preform basic crud operations

---
+++ controllers/productController.js
@@ -0,0 +1,75 @@
+import "express-async-errors"
+
+import Product from "../models/ProductModel.js"
+import { nanoid } from "nanoid"
+import { StatusCodes } from "http-status-codes"
+
+
+let products = [
+    {id:nanoid(), name:"apple", quantity:2},
+    {id:nanoid(), name:"grape", quantity:23}
+]
+
+
+export const getAllProducts = async  (req, res) => {
+    const products = await Product.find({})
+    res.status(StatusCodes.OK).json({products})
+}
+
+
+
+
+
+export const createProduct = async (req, res) => {
+    const { name, quantity } = req.body
+    const product = await Product.create(req.body)
+    res.status(200).json({product})
+}
+
+
+
+
+
+export const getProduct = async (req, res) => {
+    const { id } = req.params
+    // const product = products.find((product) => product.id === id)
+    const product = Product.findById(id)
+
+
+    if(!product) {
+        return res.status(404).json({ msg: `No product with an id of ${id}` })
+    }
+    res.status(200).json({ product })
+}
+
+
+
+
+
+export const updateProduct = async (req, res) => {
+    const { id } = req.params
+
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body, { new:true })
+ 
+    if(!updatedProduct) {
+        return res.status(404).json({ msg: `No product with id ${id}` })
+    }
+
+    res.status(200).json({ msg: "product updated", product:updatedProduct })
+}
+
+
+
+
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params
+
+    const removedProduct = await Product.findByIdAndDelete(id)
+
+    if(!removedProduct) {
+        return res.status(404).json({ msg: `No product with id ${id}` })
+    }
+
+    res.status(200).json({ msg: "product removed" })
+}
