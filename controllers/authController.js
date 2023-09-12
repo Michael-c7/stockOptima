9c1b56c180e3030a90865a6fcb877f1c7276d449
Michael Carr <mcarr2468@gmail.com>
2023-09-12 09:26:25 -0500

    setup jwt and http only cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes"
 import User from "../models/UserModel.js"
 import { comparePassword, hashPassword } from "../utils/passwordUtils.js"
 import { UnauthenticatedError } from "../errors/customError.js"
+import { createJWT } from "../utils/tokenUtils.js"
 
 
 
@@ -23,5 +24,17 @@ export const login = async (req, res) => {
     )
 
     if(!isValidUser) throw new UnauthenticatedError("invalid credentials")
-    res.send("login")
+
+
+    const token = createJWT({ userId: user._id, role: user.role })
+    // expires time has to be in milliseconds
+    const oneDay = 1000 * 60* 60 * 24
+
+    res.cookie("token", token, {
+        httpOnly:true,
+        expires:new Date(Date.now() + oneDay),
+        secure:process.env.NODE_ENV === "production",
+    })
+
+    res.status(StatusCodes.OK).json({msg:"user logged in"})
 }
