3f8a23366958f3255d1bf3cab1f9bbf2657450e3
Michael Carr <mcarr2468@gmail.com>
2023-09-15 18:33:54 -0500

    add defaultValue as a property for the input

--- client/components/InputContainer.jsx
+++ client/components/InputContainer.jsx
@@ -1,9 +1,9 @@
 
-const InputContainer = ({ type, name, labelText }) => {
+const InputContainer = ({ type, name, labelText, defaultValue }) => {
   return (
     <div className="flex flex-col text-left my-4">
         <label htmlFor={name} className='font-light capitalize mb-1'>{labelText || name}</label>
-        <input type={type} id={type} name={name} required className="border rounded border-gray-200 p-1 w-full"/>
+        <input type={type} id={type} name={name} required defaultValue={defaultValue ? defaultValue : ""} className="border rounded border-gray-200 p-1 w-full"/>
     </div>
   )
 }
