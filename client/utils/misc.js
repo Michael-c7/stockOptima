e5464816f2cf739dbf91f7a1b15cbea51219cbf1
Michael Carr <mcarr2468@gmail.com>
2023-09-15 23:39:44 -0500

    create SKUGenerator

--- client/utils/misc.js
+++ client/utils/misc.js
@@ -1,3 +1,5 @@
+import { nanoid } from "nanoid";
+
 /**
  * 
  * @param {Number} bytes - a number, 1 megabyte is equal to 1,048,576 bytes. 
@@ -7,3 +9,20 @@ export function bytesToMegabytes(bytes) {
     return (bytes / (1024 * 1024)).toFixed(2); // Returns the result with 2 decimal places
   }
   
+
+
+
+/**
+ * 
+ * @param  {...any} inputs Example of inputs: "apple","fruit", 12, 5
+ * @returns 
+ */
+export const SKUGenerator = (...inputs) => {
+  let result = "";
+  
+  for (const input of inputs) {
+    result += (input.toString().substring(0, 3) + nanoid().substring(0, 5));
+  }
+
+  return result
+}
