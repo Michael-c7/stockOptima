341add0ad7f6827e96320df5d50032a39b4e0420
Michael Carr <mcarr2468@gmail.com>
2023-09-19 17:12:43 -0500

    redesign the filter and its styles to fit the searchContainer better

--- client/components/SearchContainer.jsx
+++ client/components/SearchContainer.jsx
@@ -1,195 +1,124 @@
 import React from 'react'
-import { Popover, Disclosure } from '@headlessui/react'
+import { Popover } from '@headlessui/react'
 
-import {
-  HiChevronDown,
-  HiX,
- } from "react-icons/hi"
+import { HiChevronDown } from "react-icons/hi"
+ import { IoFilter } from "react-icons/io5"
+ import { FiSearch } from "react-icons/fi"
+
 import { PRODUCT_SORT_BY } from '../../utils/constants'
-
-
+import MinMaxInputRange from './MinMaxInputRange'
+import CombinedSearchInput from './CombinedSearchInput'
 
 
 const SearchContainer = () => {
+  let searchCategories = [
+    "name",
+    "category",
+    "location",
+    "sku",
+    "description",
+  ]
+
+
 
 
   return (
-    <div className='pb-4 flex flex-row items-center relative'>
-        <input placeholder='Search' className='border rounded border-gray-200 p-2 rounded-r-none'/>
-        
-        <Popover>
-          {/* open filters menu button */}
-          <Popover.Button className='border rounded border-gray-200 py-2 px-6 border-l-0 rounded-l-none flex flex-row items-center text-gray-700'>
-            Filter
-            <span className='flex flex-row items-center'>
-              <HiChevronDown/>
-            </span>
-          </Popover.Button>
-          {/* Filter Menu */}
-          <Popover.Panel className="absolute z-10 bg-white p-4 shadow-lg rounded">
-            <form>
-              <div className='flex justify-between space-x-2 relative'>
-                <h2 className='text-lg'>Filter by:</h2>
-                <Popover.Button className="text-lg">
-                  <HiX/>
+    <form className='pb-4 flex flex-col relative'>
+      {/* search input */}
+        <div className='flex items-center border rounded border-gray-200 p-2 rounded-r-none'>
+          <FiSearch className=' text-gray-400 mr-1 relative'/>
+          <input placeholder='Search' className='w-full outline-0'/>
+        </div>
+        {/* container for filter icon / text & filters */}
+        <div className='flex lg:flex-row flex-col lg:items-center items-start  mt-4'>
+        {/* Filter icon & text */}
+          <div className='flex relative lg:mr-4 mr-0 lg:mb-0 mb-4'>
+            <IoFilter className='text-lg top-[4px] relative mr-1'/>
+            <span className='text-lg'>Filter</span>
+          </div>
+          {/* filters container*/}
+          <div className='flex lg:flex-row flex-col lg:space-x-4 space-x-0 lg:space-y-0 space-y-4 '>
+            {/* Combined Search*/}
+            <Popover className="relative border rounded border-gray-200 p-2 rounded-r-none">
+                <Popover.Button className="flex justify-between items-center w-full">
+                  <span>Combined Search</span>
+                  <HiChevronDown className='text-lg'/>
                 </Popover.Button>
-              </div>
+              <Popover.Panel className="absolute z-10 bg-white p-4 shadow-md top-12 space-y-4 w-full">
+              {/* <input className='border rounded border-gray-600 p-2 rounded-r-none' value={testText} onChange={(e) => setTestText(e.target.value)}/> */}
+              
+                {/* combined search inputs */}
+                {searchCategories.map((item, index) => {
+                  let capitalizeFirstLetterString = item[0].toUpperCase() + item.slice(1);
+                  return (
+                    <CombinedSearchInput {...{item: capitalizeFirstLetterString, itemId:item}} key={index}/>
+                  )
+                })}
+              </Popover.Panel>
+            </Popover>
 
-              <hr className='bottom-border'/>
+            {/* Price */}
+            <Popover className="relative border rounded border-gray-200 p-2 rounded-r-none">
+                <Popover.Button className="flex justify-between items-center w-full">
+                  <span>Price</span>
+                  <HiChevronDown className='text-lg'/>
+                </Popover.Button>
+                <Popover.Panel className="absolute z-10 bg-white p-4 shadow-md top-12 left-0">
+                  <MinMaxInputRange {...{rangeName:"Price", minId:"priceMin", maxId: "priceMax"}}/>
+                </Popover.Panel>
+            </Popover>
 
-              {/* all filters */}
-              <div className='mx-2 space-y-4'>
-                {/* multiselect search includes */}
-                <Disclosure defaultOpen={true}>
-                  <Disclosure.Button className="flex flex-row items-center justify-between w-full ">
-                    <span>Text search includes</span>
-                    <HiChevronDown className=' text-lg'/>
-                  </Disclosure.Button>
-                    <Disclosure.Panel className="mx-4 space-y-1">
-                      <div className='flex items-center py-[2px] '>
-                          <label htmlFor="name" className='mr-1'>Name</label>
-                          {/* should be checked={true} by default */}
-                          <input type="checkbox" id="name" name="name" value="Bike"  /> 
-                        </div>
-                        <div className='flex items-center py-[2px]'>
-                          <label htmlFor="category" className='mr-1'>Category</label>
-                          <input type="checkbox" id="category" name="category" value="Bike"/>
-                        </div>
-                        <div className='flex items-center py-[2px]'>
-                          <label htmlFor="vehicle1" className='mr-1'>Location</label>
-                          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-                        </div>
-                        <div className='flex items-center py-[2px]'>
-                          <label htmlFor="vehicle1" className='mr-1'>SKU</label>
-                          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-                        </div>
-                        <div className='flex items-center py-[2px]'>
-                          <label htmlFor="vehicle1" className='mr-1'>Description</label>
-                          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-                      </div>
-                    </Disclosure.Panel>
-                </Disclosure>
-                
-                {/* Price */}
-                <Disclosure defaultOpen={true}>
-                  <Disclosure.Button className="flex flex-row items-center justify-between w-full">
-                    <span>Price</span>
-                    <HiChevronDown className=' text-lg'/>
-                  </Disclosure.Button>
-                    <Disclosure.Panel>
-                    <div className='flex flex-row mx-4 p-1 space-x-4 relative after:w-[calc(100%+5rem)] after:left-[calc(-2.5rem)]'>
-                    <div className='flex flex-col py-1'>
-                      <label htmlFor="createdByMin">Min</label>
-                      <input type="text" id="createdByMin" name="createdByMin" className='border p-1 border-gray-300 rounded' placeholder=' min'/>
-                    </div>
-                    <div className='flex flex-col py-1'>
-                      <label htmlFor="createdByMax">Max</label>
-                      <input type="text" id="createdByMax" name="createdByMax" className='border p-1 border-gray-300 rounded' placeholder=' max'/>
-                    </div>
-                  </div>
-                    </Disclosure.Panel>
-                </Disclosure>
+            {/* Quantity */}
+            <Popover className="relative border rounded border-gray-200 p-2 rounded-r-none">
+                <Popover.Button className="flex justify-between items-center w-full">
+                  <span>Quantity</span>
+                  <HiChevronDown className='text-lg'/>
+                </Popover.Button>
+                <Popover.Panel className="absolute z-10 bg-white p-4 shadow-md top-12 left-0">
+                  <MinMaxInputRange {...{rangeName:"Quantity", minId:"quantityMin", maxId: "quantityMax"}}/>
+                </Popover.Panel>
+            </Popover>
 
+            {/* Value */}
+            <Popover className="relative border rounded border-gray-200 p-2 rounded-r-none">
+                <Popover.Button className="flex justify-between items-center w-full">
+                  <span>Value</span>
+                  <HiChevronDown className='text-lg'/>
+                </Popover.Button>
+                <Popover.Panel className="absolute z-10 bg-white p-4 shadow-md top-12 left-0">
+                  <MinMaxInputRange {...{rangeName:"Value", minId:"valueMin", maxId: "valueMax"}}/>
+                </Popover.Panel>
+            </Popover>
 
-                {/* Quantity */}
-                <Disclosure defaultOpen={true}>
-                  <Disclosure.Button className="flex flex-row items-center justify-between w-full">
-                    <span>Quantity</span>
-                    <HiChevronDown className=' text-lg'/>
-                  </Disclosure.Button>
-                    <Disclosure.Panel>
-                    <div className='flex flex-row mx-4 p-1 space-x-4 relative   after:w-[calc(100%+5rem)] after:left-[calc(-2.5rem)]'>
-                      <div className='flex flex-col py-1'>
-                        <label htmlFor="createdByMin">Min</label>
-                        <input type="text" id="createdByMin" name="createdByMin" className='border p-1 border-gray-300 rounded' placeholder=' min'/>
-                      </div>
-                      <div className='flex flex-col py-1'>
-                        <label htmlFor="createdByMax">Max</label>
-                        <input type="text" id="createdByMax" name="createdByMax" className='border p-1 border-gray-300 rounded' placeholder=' max'/>
-                      </div>
-                  </div>
-                    </Disclosure.Panel>
-                </Disclosure>
+            {/* Created By */}
+            <Popover className="relative border rounded border-gray-200 p-2 rounded-r-none">
+            <Popover.Button className="flex justify-between items-center w-full">
+                  <span>Created By</span>
+                  <HiChevronDown className='text-lg'/>
+                </Popover.Button>
+                <Popover.Panel className="absolute z-10 bg-white p-4 shadow-md top-12 left-0">
+                  <MinMaxInputRange {...{rangeName:"Created by", minId:"createdByMin", maxId: "createdByMax", inputType:"date"}}/>
+                </Popover.Panel>
+            </Popover>
 
-
-                {/* Value */}
-                <Disclosure defaultOpen={true}>
-                  <Disclosure.Button className="flex flex-row items-center justify-between w-full">
-                    <span>Value</span>
-                    <HiChevronDown className=' text-lg'/>
-                  </Disclosure.Button>
-                    <Disclosure.Panel>
-                    <div className='flex flex-row mx-4 p-1 space-x-4'>
-                    <div className='flex flex-col py-1'>
-                      <label htmlFor="createdByMin">Min</label>
-                      <input type="text" id="createdByMin" name="createdByMin" className='border p-1 border-gray-300 rounded' placeholder=' min'/>
-                    </div>
-                    <div className='flex flex-col py-1'>
-                      <label htmlFor="createdByMax">Max</label>
-                      <input type="text" id="createdByMax" name="createdByMax" className='border p-1 border-gray-300 rounded' placeholder=' max'/>
-                    </div>
-                  </div>
-                    </Disclosure.Panel>
-                </Disclosure>
-
-
-                {/*
-                https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date
-                theirs a max and min prop for the input date, use this to restrict the selection when another date input is selected eg: user inputs a date into the min input, use this min as the min prop the max so you dont get, and the inverse as well so you dont get any weird bugs.
-                the min and max properties take a string in format yyyy-mm-dd eg: 1979-12-31
-                */}
-
-                {/* Created by */}
-                <Disclosure defaultOpen={true}>
-                  <Disclosure.Button className="flex flex-row items-center justify-between w-full">
-                    <span>Created By</span>
-                    <HiChevronDown className='text-lg'/>
-                  </Disclosure.Button>
-                    <Disclosure.Panel>
-                    <div className='flex flex-row mx-4 p-1 space-x-4'>
-                    <div className='flex flex-col'>
-                      <label htmlFor="createdByMin">Min</label>
-                      <input type="date" id="createdByMin" name="createdByMin" className='border p-1 border-gray-300 rounded'></input>
-                    </div>
-                    <div className='flex flex-col'>
-                      <label htmlFor="createdByMax">Max</label>
-                      <input type="date" id="createdByMax" name="createdByMax" className='border p-1 border-gray-300 rounded'></input>
-                    </div>
-                  </div>
-                    </Disclosure.Panel>
-                </Disclosure>
-
-
-
-                {/* SORT */}
-                <Disclosure defaultOpen={true}>
-                  <Disclosure.Button className="flex flex-row items-center justify-between w-full">
-                    <span>Sort</span>
-                    <HiChevronDown className='text-lg'/>
-                  </Disclosure.Button>
-                    <Disclosure.Panel>
-                      <select name="sort" id="sort" className='border border-gray-300 rounded mx-4 p-2'>
-                        {Object.keys(PRODUCT_SORT_BY).map((value, index) => {
-                          let text = Object.values(PRODUCT_SORT_BY)[index]
-                          return (
-                            <option value={value} key={index}>{text}</option>
-                          )
-                        })}
-                      </select>
-                    </Disclosure.Panel>
-                </Disclosure>
-              </div>
-
-              <hr className='bottom-border'/>
-
-              <div className='flex justify-end'>
-                <button className='text-blue-400 mr-4'>Reset Filter</button>
-                <button type='submit' className='btn-main py-2 px-6'>Apply</button>
-              </div>
-            </form>
-          </Popover.Panel>
-        </Popover>
-    </div>
+            {/* SORT */}
+            <Popover className="relative border rounded border-gray-200 p-2 rounded-r-none">
+                <Popover.Button className="flex justify-between items-center w-full">
+                  <span>Sort</span>
+                  <HiChevronDown className='text-lg'/>
+                </Popover.Button>
+                <Popover.Panel className="absolute z-10 bg-white p-4 shadow-md top-12 left-0 w-40 space-y-4">
+                {Object.entries(PRODUCT_SORT_BY).map(([key, value]) => {
+                  let capitalizeFirstLetterString = value[0].toUpperCase() + value.slice(1);
+                  return (
+                    <CombinedSearchInput {...{item: capitalizeFirstLetterString, inputType:"radio", itemId:"productSort"}} key={key}/>
+                  )
+                })}
+                </Popover.Panel>
+            </Popover>
+          </div>
+        </div>
+    </form>
   )
 }
 
