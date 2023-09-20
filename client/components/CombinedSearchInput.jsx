db5739bc99c201c262452ffb5e87b5d8c07d8deb
Michael Carr <mcarr2468@gmail.com>
2023-09-20 11:17:01 -0500

    add pointer cursor when you hover over the input or label

--- client/components/CombinedSearchInput.jsx
+++ client/components/CombinedSearchInput.jsx
@@ -5,9 +5,9 @@ const CombinedSearchInput = ({item, inputType = "checkbox", itemId}) => {
 
     return (
         <>
-            <label htmlFor={item} className='flex justify-between items-center py-[2px]'>
-            <span className='mr-1'>{item}</span>
-            <input type={inputType} id={item} name={itemId} value={item} /> 
+            <label htmlFor={item} className='flex justify-between items-center py-[2px] hover:cursor-pointer'>
+                <span className='mr-1'>{item}</span>
+                <input type={inputType} id={item} name={itemId} value={item}  className='hover:cursor-pointer'/> 
             </label>
         </>
     )
