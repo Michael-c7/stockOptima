a8428f9d1a0d84b8f05df6b9851fdce7cf010dc5
Michael Carr <mcarr2468@gmail.com>
2023-09-21 12:32:19 -0500

    add function to separate numbers with commas and add tooltips for the action in the table

--- client/components/ProductContainer.jsx
+++ client/components/ProductContainer.jsx
@@ -5,8 +5,11 @@ import {
     HiPencilAlt,
     HiTrash,
    } from "react-icons/hi"
-import { truncateStringWithEllipsis } from '../utils/misc'
+import { addCommasToNumber, truncateStringWithEllipsis } from '../utils/misc'
 import DeleteProductModal from './DeleteProductModal'
+
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
 
 const ProductContainer = ({item, index}) => {
   const { _id } = item
@@ -14,24 +17,45 @@ const ProductContainer = ({item, index}) => {
   let textLimitInCharacters = 15
   let [isDeleteProductModalOpen, setDeleteProductModalIsOpen] = React.useState(false)
 
+  addCommasToNumber
   return (
     <tr className='w-full border-b-2'>
         <td className=' px-2'>{index + 1}</td>
         <td className='py-5'>{truncateStringWithEllipsis(item.name, textLimitInCharacters)}</td>
         <td className='px-2'>{truncateStringWithEllipsis(item.category, textLimitInCharacters)}</td>
-        <td className='px-2'>${truncateStringWithEllipsis(item.price, textLimitInCharacters)}</td>
-        <td className='px-2'>{truncateStringWithEllipsis(item.quantity, textLimitInCharacters)}</td>
-        <td className='px-2'>${truncateStringWithEllipsis(item.value, textLimitInCharacters)}</td>
+        <td className='px-2'>${addCommasToNumber(truncateStringWithEllipsis(item.price, textLimitInCharacters))}</td>
+        <td className='px-2'>{addCommasToNumber(truncateStringWithEllipsis(item.quantity, textLimitInCharacters))}</td>
+        <td className='px-2'>${addCommasToNumber(truncateStringWithEllipsis(item.value, textLimitInCharacters))}</td>
         <td className='px-2'>{truncateStringWithEllipsis(item.location, textLimitInCharacters)}</td>
         <td className='flex items-center mt-5 relative space-x-2 px-2 text-gray-600'>
             {/* single page link */}
-            <Link to={`/dashboard/singleProduct/${_id}`}><HiEye/></Link>
+            <Link to={`/dashboard/singleProduct/${_id}`} data-tooltip-id="single-product-tooltip-id"><HiEye/></Link>
+            {/* single page tooltip */}
+            <ReactTooltip
+              id="single-product-tooltip-id"
+              place="top"
+              content="View Product Details"
+            />
+            
             {/* edit page link */}
-            <Link to={`/dashboard/editProduct/${_id}`}><HiPencilAlt/></Link>
+            <Link to={`/dashboard/editProduct/${_id}`} data-tooltip-id="edit-product-tooltip-id"><HiPencilAlt/></Link>
+            {/* single page tooltip */}
+            <ReactTooltip
+              id="edit-product-tooltip-id"
+              place="top"
+              content="Edit Product Details"
+            />
 
             <DeleteProductModal {...{isDeleteProductModalOpen, setDeleteProductModalIsOpen, _id}}/>
             {/* Delete button */}
-            <button onClick={() => setDeleteProductModalIsOpen(true)}><HiTrash/></button>
+            <button onClick={() => setDeleteProductModalIsOpen(true)} data-tooltip-id="delete-product-tooltip-id"><HiTrash/></button>
+        
+            {/* single page tooltip */}
+            <ReactTooltip
+              id="delete-product-tooltip-id"
+              place="top"
+              content="Delete Product"
+            />
         </td>
   </tr>
   )
