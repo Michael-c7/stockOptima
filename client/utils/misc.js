fc20f1fa464d1f934662eec794e0650dfedf6665
Michael Carr <mcarr2468@gmail.com>
2023-09-21 12:24:44 -0500

    add function to returns a number formatted with commas as thousands

--- client/utils/misc.js
+++ client/utils/misc.js
@@ -39,3 +39,16 @@ export const truncateStringWithEllipsis = (property, textLimitInCharacters = 100
   ? `${property.slice(0, textLimitInCharacters)}...`
   : property
 )
+
+/**
+ * 
+ * @param {Number} number 
+ * @returns if typeof number returns a number formatted with commas as thousands separators based on the user's locale setting. Else if not a number return the number unchanged
+ */
+export function addCommasToNumber(number) {
+  if (typeof number !== "number") {
+    return number; // Return unchanged if it's not a number
+  }
+  // toLocaleString automatically formats your number with commas as thousands separators based on the user's locale setting
+  return number.toLocaleString();
+}
