04f9c95ba4e7cc6113f3d897b79ccec55e922608
Michael Carr <mcarr2468@gmail.com>
2023-09-21 15:05:20 -0500

    update Link to prop from / to /dashboard so it goes to the dashboard also made the alt text more descriptive

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -10,20 +10,20 @@ const Error = () => {
   if(error.status === 404) {
     return (
       <div className="min-h-[100vh] text-center flex flex-col justify-center items-center">
-        <img src={pageNotFound} className="w-[90vw] max-w-[600px]" alt="not found"/>
+        <img src={pageNotFound} className="w-[90vw] max-w-[600px]" alt="404 page not found"/>
         <h2 className="text-4xl mt-8">Page not found.</h2>
         <p className="text-gray-500 mt-2 mb-4">Apologies, but it seems like we couldn't find the page you're looking for.</p>
-        <Link to="/" className="btn-main py-4 px-6">Go home</Link>
+        <Link to="/dashboard" className="btn-main py-4 px-6">Go home</Link>
       </div>
     )
   }
 
   return (
     <div className="min-h-[100vh] text-center flex flex-col justify-center items-center">
-      <img src={serverDown} className="w-[90vw] max-w-[600px]" alt="not found"/>
+      <img src={serverDown} className="w-[90vw] max-w-[600px]" alt="An Error has Occurred"/>
       <h2 className="text-4xl mt-8">An Error has Occurred.</h2>
       <p className="text-gray-500 mt-2 mb-4">We apologize for any disruption this may have caused to your experience.</p>
-      <Link to="/" className="btn-main py-4 px-6">Go home</Link>
+      <Link to="/dashboard" className="btn-main py-4 px-6">Go home</Link>
     </div>
   )
 }
