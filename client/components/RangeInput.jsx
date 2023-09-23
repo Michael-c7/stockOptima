66f8cae9a04403f5f07d9452870d1e07a9d38d8d
Michael Carr <mcarr2468@gmail.com>
2023-09-23 16:44:27 -0500

    add maxValue prop for the max value

--- client/components/RangeInput.jsx
+++ client/components/RangeInput.jsx
@@ -3,7 +3,7 @@ import { addCommasToNumber } from '../utils/misc'
 
 
 
-const RangeInput = ({rangeName, id, value, onChange, }) => {
+const RangeInput = ({rangeName, id, value, onChange, maxValue }) => {
   return (
     <>
         <div className='flex items-center justify-between w-72'>
@@ -11,7 +11,7 @@ const RangeInput = ({rangeName, id, value, onChange, }) => {
             <button className='text-blue-500' onClick={() => onChange(0)}>Reset</button>
         </div>
         <div className='flex space-x-2 py-2 items-center'>
-            <input type="range" id={id} name={id} value={value} max={1000} onChange={(e) => onChange(e.target.value)} className='border p-2 border-gray-300 rounded w-full'/>
+            <input type="range" id={id} name={id} value={value} max={maxValue} onChange={(e) => onChange(e.target.value)} className='border p-2 border-gray-300 rounded w-full'/>
         </div>
     </>
   )
