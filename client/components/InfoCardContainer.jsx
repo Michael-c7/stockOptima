9f85c73b44194289d8fe34339632ecad788fa06b
Michael Carr <mcarr2468@gmail.com>
2023-09-22 21:27:01 -0500

    create component for info cards at the top of stats page

---
+++ client/components/InfoCardContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { addCommasToNumber, truncateStringWithEllipsis } from '../utils/misc'
+
+
+
+export const InfoCardContainer = ({icon, headingText, infoNumber}) => {
+    let textLimitInCharacters = 200
+  
+    return (
+      <div className="flex-1 bg-white p-8 space-y-6 rounded border-b-4 border-b-green-500">
+        <header className='flex justify-between items-center text-3xl '>
+          <span>{addCommasToNumber(truncateStringWithEllipsis(infoNumber, textLimitInCharacters))}</span>
+          <span className='bg-green-500 p-4 rounded text-white ml-2'>{icon}</span>
+        </header>
+        <h5 className='text-xl text-gray-700'>{truncateStringWithEllipsis(headingText, textLimitInCharacters)}</h5>
+      </div>
+    )
+  }
--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,51 +1,179 @@
-import React from 'react'
+import { Fragment, useState } from 'react'
 
-import { HiShoppingCart,HiCurrencyDollar,HiBan, HiDocumentText } from "react-icons/hi"
+import { 
+  HiShoppingCart,
+  HiCurrencyDollar,
+  HiBan,
+  HiDocumentText,
+  HiCheck,
+} from "react-icons/hi"
+import { HiChevronUpDown } from "react-icons/hi2"
+
+
+import { addCommasToNumber, truncateStringWithEllipsis } from '../../utils/misc'
+import { useLoaderData } from 'react-router-dom';
+import { Listbox, Transition } from '@headlessui/react'
+import customFetch from "../../utils/customFetch"
+import { InfoCardContainer } from '../../components/InfoCardContainer';
+import StatsChartContainer from '../../components/StatsChartContainer';
+
+
+
+
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/products/stats');
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+}
+
+
+
+
+
+
+
 
 const Stats = () => {
+  const { defaultStats, chartData } = useLoaderData()
+  // console.log({defaultStats, chartData} )
+
+  const cardInfo = [
+    {
+      icon:<HiShoppingCart/>,
+      headingText:"Total Products",
+      infoNumber:defaultStats.totalProducts,
+    },
+    {
+      icon:<HiCurrencyDollar/>,
+      headingText:"Total Store Value",
+      infoNumber:defaultStats.totalStoreValue,
+    },
+    {
+      icon:<HiBan/>,
+      headingText:"Out of Stock",
+      infoNumber:defaultStats.outOfStock,
+    },
+    {
+      icon:<HiDocumentText/>,
+      headingText:"All Categories",
+      infoNumber:defaultStats.allCategories,
+    },
+  ]
+
+  const statOptions = [
+    {
+      text:"Top 5 Categories",
+    },
+    {
+      text:"Monthly Products",
+    },
+    {
+      text:"Price Breakdowns",
+    },
+    {
+      text:"Highest Quantity products",
+    }
+  ]
+
+
+  const people = [
+    { 
+      name: 'Most popular Categories',
+      text: "mostPopularCategories",
+    },
+    { 
+      name: 'Monthly Products',
+      text: "monthlyProducts"
+    },
+    { 
+      name: 'Highest Quantity products',
+      text: "highestQuantityProducts"
+    },
+    { 
+      name: 'Lowest Quantity products',
+      text: "lowestQuantityProducts"
+    },
+
+  ]
+
+
+  let [selected, setSelected] = useState(people[0])
+
+  console.log(selected)
+
+
   return (
-    <section>
-      <div className='flex flex-row space-x-6'>
-        <div className='flex flex-row items-center bg-white p-4 drop-shadow-sm'>
-          <div className='text-4xl mr-2'>
-            <HiShoppingCart/>
-          </div>
-          <div>
-              <h2>Total Products</h2>
-              <p>1</p>
-          </div>
-        </div>
-        <div className='flex flex-row items-center bg-white p-4 drop-shadow-sm'>
-          <div className='text-4xl mr-2'>
-            <HiCurrencyDollar/>
-          </div>
-          <div>
-              <h2>Total Store Value</h2>
-              <p>1</p>
-          </div>
-        </div>
-        <div className='flex flex-row items-center bg-white p-4 drop-shadow-sm'>
-          <div className='text-4xl mr-2'>
-            <HiBan/>
-          </div>
-          <div>
-              <h2>Out of Stock</h2>
-              <p>1</p>
-          </div>
-        </div>
-        <div className='flex flex-row items-center bg-white p-4 drop-shadow-sm'>
-          <div className='text-4xl mr-2'>
-            <HiDocumentText/>
-          </div>
-          <div>
-              <h2>All Categories</h2>
-              <p>1</p>
-          </div>
-        </div>
+    <section className='space-y-6'>
+      <div className='flex flex-wrap gap-4'>
+        {cardInfo.map(({icon, headingText, infoNumber, color}, index) => {
+          return (
+            <InfoCardContainer {...{icon, headingText, infoNumber, color}} key={index}/>
+          )
+        })}
       </div>
 
-      <div>
-        bar / pie carts here
+      <div className=''>
+        <div className=''>
+          <h2 className='text-center text-3xl  mt-16'>{selected.name}</h2>
+          {/* <div>put select from headless ui here / listbox</div> */}
+            <Listbox value={selected} onChange={setSelected} className="w-72 m-auto  z-40">
+              <div className="relative my-4">
+                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-green-300 sm:text-sm">
+                  <span className="block truncate">{selected.name}</span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <HiChevronUpDown
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+                <Transition
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {people.map((person, personIdx) => (
+                      <Listbox.Option
+                        key={personIdx}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            active ? 'bg-green-100 text-green-900' : 'text-gray-900'
+                          }`
+                        }
+                        value={person}
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? 'font-medium' : 'font-normal'
+                              }`}
+                            >
+                              {person.name}
+                            </span>
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
+                                <HiCheck className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </Listbox>
+        </div>
+        <div className='relative'>
+            <StatsChartContainer {...{selectedChart:selected.text, chartData}}/>
+        </div>
       </div>
 
       
