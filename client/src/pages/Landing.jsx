fe661e1028fc335dce569d2de01294e6d3e8c792
Michael Carr <mcarr2468@gmail.com>
2023-09-23 20:42:05 -0500

    replace placeholder image with a real image

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+import landingPageImage from "../assets/images/landingPageImage.png"
+
 const Landing = () => {
   let headingText = ", Your Effortless Inventory Management Solution"
   let descriptionText = "Are you tired of struggling with manual spreadsheets and complex inventory management processes? Look no further! StockOptima is here to revolutionize the way you handle your inventory."
   
-  // the current image is a placeholder, make sure to put either an image or a video of the real dashboard / site.
-  let imgDisplay = "https://cdn.dribbble.com/users/584216/screenshots/14488488/media/fce03b7973f46e54c50ca76c22cd0479.png"
 
   return (
     <div className="grid place-items-center h-[100vh]">
@@ -28,9 +28,8 @@ const Landing = () => {
           </div>
         </div>
 
-        <div className="mt-10 w-full">
-          {/* make sure when you've finished the actual dashboard & populated it w/ data to replace the placeholder here w/ real screenshot */}
-          <img src={imgDisplay} alt="dashboard of stock optima" className="rounded-sm max-h-[500px] w-full object-cover drop-shadow"/>
+        <div className="mt-10 w-full shadow-lg">
+          <img src={landingPageImage} alt="dashboard of stock optima" className="rounded-sm max-h-[500px] w-full object-cover drop-shadow"/>
         </div>
       </div>
 
