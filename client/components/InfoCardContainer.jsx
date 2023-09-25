f81a5bab1bf8ae11d4714d8ffa213f53505f5d5d
Michael Carr <mcarr2468@gmail.com>
2023-09-25 15:52:21 -0500

    add a small shadow fir the info cards

--- client/components/InfoCardContainer.jsx
+++ client/components/InfoCardContainer.jsx
@@ -7,7 +7,7 @@ export const InfoCardContainer = ({icon, headingText, infoNumber}) => {
     let textLimitInCharacters = 200
   
     return (
-      <div className="flex-1 bg-white p-8 space-y-6 rounded border-b-4 border-b-green-500">
+      <div className="flex-1 bg-white p-8 space-y-6 rounded border-b-4 border-b-green-500 shadow-sm">
         <header className='flex justify-between items-center text-3xl '>
           <span>{addCommasToNumber(truncateStringWithEllipsis(infoNumber, textLimitInCharacters))}</span>
           <span className='bg-green-500 p-4 rounded text-white ml-2'>{icon}</span>
